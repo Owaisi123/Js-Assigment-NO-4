b0c174483bea49a106377946d0cd532efb6667d0
Owaisi123 <165272983+Owaisi123@users.noreply.github.com>
2024-08-29 13:27:43 +0500

    Add files via upload
    Js Assigment No 4

---
+++ Assigment 4/script.js
@@ -0,0 +1,41 @@
+//                Assigment         4              //
+
+
+
+// Q No 1:
+// Ans: 1
+// var myName="Owais";
+// alert(myName);
+// var myName$="Owais";
+// alert(myName);
+// var my_Name="Owais";
+// alert(myName);
+
+
+
+// Q No 2:
+// Ans : 2 
+// Legals types to declare variable:
+
+//  1:   var my_Name="";
+//  2: var myName="";
+//  3 var my_Name$="";
+//  4: var my1Name="";
+//  5: var my$Name1="";
+
+
+//  Illegal Types to declare variable:
+
+//  1: var my-name="";
+//  2: var 1myName="";
+//  3: var @@@@@@@="";
+//  4: var 88888888="";
+//  5: var my-$Name="";
+
+
+
+
+
+// Q No 3:
+// Ans No 3:
+// document.write("<b>" + "Rules for naming JS variable." + "</b>" + "<br><br><br>" + "Variable names can only contain, numbers, $ and _ . For example : $my_1stVariable." + "<br>" + "Variables must begin with a letter, $ or _ For example : $name,_name or name."+"<br>"+"Variable names are case sensitive."+"<br>"+"Variable names should'nt be JS Keywords.");
